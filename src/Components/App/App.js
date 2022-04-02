5c56f64911bdc087342f1481ef3c0fa882c0c7d7
Petar Partaloski <ppartaloski@yahoo.com>
2022-04-02 14:56:30 +0200

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,186 @@
+import React, {Component} from "react";
+import LibraryService from "../../Repository/LibraryService";
+import Navigation from '../../Components/Navigation/Navigation'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import BookAdd from '../Books/BookAdd/BookAdd'
+import BookEdit from '../Books/BookEdit/BookEdit'
+import Books from '../Books/BooksList/BooksList'
+import Categories from '../Categories/Categories'
+
+
+
+class App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            books: [],
+            authors: [],
+            countries: [],
+            categories: [],
+            categoriesCount: [],
+            selectedBook: {}
+        }
+    }
+
+    componentDidMount() {
+        this.loadBooks();
+        this.loadAuthors();
+        this.loadCountries();
+        this.loadCategories();
+        this.loadCategoriesWithCount();
+    }
+
+    loadBooks = () =>{
+        LibraryService.fetchBooks()
+            .then((data) => {
+                this.setState({
+                    books: data.data
+                })
+            });
+    }
+
+
+    loadAuthors = () =>{
+        LibraryService.fetchAuthors()
+            .then((data) => {
+                this.setState({
+                    authors: data.data
+                })
+            });
+    }
+
+    loadCountries = () => {
+        LibraryService.fetchCountries()
+            .then((data) => {
+                this.setState({
+                    countries: data.data
+            })
+        })
+    }
+
+    loadCategories = () =>{
+        LibraryService.fetchCategories()
+            .then((data) => {
+                this.setState({
+                    categories: data.data
+                })
+            });
+    }
+
+    loadCategoriesWithCount = () =>{
+        LibraryService.fetchCategoriesCount()
+            .then((data) => {
+                this.setState({
+                    categoriesCount: data.data
+                })
+            });
+    }
+
+    deleteBook = (id) =>{
+        LibraryService.deleteBook(id).then(() => {
+            this.loadBooks();
+        })
+    }
+
+    takeBook = (id) =>{
+        LibraryService.takeBook(id).then(() => {
+            this.loadBooks();
+        })
+    }
+
+    addBook = (name, author, availableCopies, category) => {
+        LibraryService.addBook(name, author, availableCopies, category).then(() => {
+            this.loadBooks();
+        })
+    }
+
+
+    bookEdit = (id, name, author, availableCopies, category) => {
+        console.log(id)
+        console.log(name)
+        console.log(author)
+        console.log(availableCopies)
+        console.log(category)
+        LibraryService.editBook(id, name, author, availableCopies, category).then(() => {
+            this.loadBooks();
+        })
+    }
+
+    getBook = (id) => {
+        LibraryService.getBook(id).then((data) => {
+            this.setState({
+                selectedBook: data.data
+            })
+        })
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navigation/>
+                <main>
+                    <div className="container">
+                        <Routes>
+                            <Route
+                                path={"/books/add"}
+                                element={
+                                    <BookAdd
+                                        authors={this.state.authors}
+                                        categories={this.state.categories}
+                                        onAddBook={this.addBook}
+                                    />}
+                            />
+
+                            <Route
+                                path={"/books/edit/:id"}
+                                element={
+                                    <BookEdit
+                                        authors={this.state.authors}
+                                        categories={this.state.categories}
+                                        book={this.state.selectedBook}
+                                        onBookEdit={this.bookEdit}
+                                    />}
+                            />
+
+                            <Route
+                                path={"/books"}
+                                element={
+                                    <Books
+                                        books={this.state.books}
+                                        onDeleteBook={this.deleteBook}
+                                        onBookEdit={this.getBook}
+                                        onTakeBook={this.takeBook}
+                                    />}
+                            />
+
+
+                            <Route
+                                path={"/"}
+                                element={
+                                    <Books
+                                        books={this.state.books}
+                                        onDeleteBook={this.deleteBook}
+                                        onBookEdit={this.getBook}
+                                        onTakeBook={this.takeBook}
+                                    />}
+                            />
+
+                            <Route
+                                path={"/categories"}
+                                element={<Categories
+                                    categories={this.state.categoriesCount}
+                                />}
+                            />
+
+                        </Routes>
+                    </div>
+                </main>
+            </Router>
+        );
+    }
+
+
+}
+
+export default App;
+
